ae5293b96cdeecb718983f8a34f903c98ea27a1a
VedantKale08 <fscocomputer@gmail.com>
2024-11-09 16:52:04 +0530

    basic changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        secondary: "#189976",
+        primary: "#083730",
       },
     },
   },
